a01a9c6f379b042d9b4f8af850904d6fcab23736
yan <yan.p@fideware.by>
2022-10-03 17:57:12 +0300

    add abstract usecase

---
+++ lib/core/usecases/usecase.dart
@@ -0,0 +1,6 @@
+import 'package:rick_and_morty_dart/core/error/failure.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class UseCase<Type, Params> {
+  Future<Either<Failure, Type>> call(Params params);
+}
